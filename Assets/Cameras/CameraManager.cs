a83ffe8a5fde195728b7940537556576c7851dd0
Masacre23 <adrianguevaralonso@gmail.com>
2017-03-07 15:53:16 +0100

    Added static cameras
    Press "V" to change the camera

---
+++ Assets/Cameras/CameraManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+    int numStaticCameras;
+    int actualCamera = 0;
+    GameObject staticCameras;
+
+	void Start () {
+        staticCameras = this.gameObject.transform.GetChild(0).gameObject;
+        numStaticCameras = staticCameras.transform.childCount;
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown("v"))
+        {
+            staticCameras.transform.GetChild(actualCamera).gameObject.SetActive(false);
+            actualCamera++;
+            if(actualCamera >= numStaticCameras)
+            {
+                actualCamera = 0;
+            }
+            staticCameras.transform.GetChild(actualCamera).gameObject.SetActive(true);
+        }
+	}
+
+    private void OnEnable()
+    {
+        GameObject.Find("Player").SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        GameObject.Find("Player").SetActive(true);
+    }
+}
