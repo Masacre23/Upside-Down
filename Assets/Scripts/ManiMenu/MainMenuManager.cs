908a09f3465c49ec3a090de80b8c88ef88e225f9
Esteban Arrua <estarr22@hotmail.com>
2017-03-09 16:48:56 +0100

    New Scene Main Menu with buttons Play, Help and Quit implemented.

---
+++ Assets/Scripts/ManiMenu/MainMenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour {
+	public Canvas m_mainMenuCanvas;
+	public Canvas m_helpCanvas;
+	public Canvas m_creditsCanvas;
+
+	public void PressPlayButton(){
+		Application.LoadLevel(0);
+	}
+
+	public void PressHelpButton(){
+		m_mainMenuCanvas.gameObject.SetActive(false);
+		m_helpCanvas.gameObject.SetActive(true);
+	}
+
+	public void PressCreditsButton(){
+		m_mainMenuCanvas.gameObject.SetActive(false);
+		m_creditsCanvas.gameObject.SetActive(true);
+	}
+
+	public void PreesQuitButton(){
+		Debug.Log("Quit");
+		Application.Quit();
+	}
+
+	public void BackMainMenu(){
+		m_mainMenuCanvas.gameObject.SetActive(true);
+		m_helpCanvas.gameObject.SetActive(false);
+		m_creditsCanvas.gameObject.SetActive(false);
+	}
+}
