6a3ef2900ca8be5c39397ea0cb1f81d8d81c63d6
Adrián Guevara Alonso <adrianguevaralonso@gmail.com>
2017-03-11 03:00:40 +0100

    End of the game implemented
    Now we can throw the bottles to the targets (previosly pressing "I" to
    create the bottles) and then the door disappear and a game over menu
    appear and we can back to the main menu

---
+++ Assets/Scripts/Objects/FinalButtons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalButtons : MonoBehaviour {
+	[SerializeField] GameObject finalDoor;
+	bool pressed = false;
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "GravityAffected" && !pressed) {
+			finalDoor.GetComponent<FinalDoor> ().Activate ();
+			pressed = true;
+		}
+	}
+}
--- Assets/Scripts/Scene/SceneManager.cs
+++ Assets/Scripts/Scene/SceneManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour {
     [SerializeField] GameObject cameras;
+	[SerializeField] GameObject gameoverPanel;
     bool b = false;
 
 	void Update () {
@@ -12,5 +13,15 @@ public class SceneManager : MonoBehaviour {
             b = !b;
             cameras.SetActive(b);
         }
+	}
+
+	public void GameOver()
+	{
+		gameoverPanel.SetActive (true);
+	}
+
+	public void BackToMenu()
+	{
+		Application.LoadLevel(1);
 	}
 }
