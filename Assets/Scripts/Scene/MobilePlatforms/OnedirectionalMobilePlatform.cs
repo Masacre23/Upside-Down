07c6dd6d914102701cf507a2d66ed6ff864bfb68
estebanarrua <estarr22@hotmail.com>
2017-03-11 11:50:42 +0100

    Add wait time to MobilePlataforms.

--- Assets/Scripts/Scene/MobilePlatforms/OnedirectionalMobilePlatform.cs
+++ Assets/Scripts/Scene/MobilePlatforms/OnedirectionalMobilePlatform.cs
@@ -6,27 +6,44 @@ public class OnedirectionalMobilePlatform : MonoBehaviour {
 
     public float mSpeed = 0.2f;
     public float mDistance = 2.0f;
+    public float mWaitTime = 0.5f;
     public Vector3 mDirection = new Vector3(1.0f, 0.0f, 0.0f);
     public bool mBoomerang = true;
 
     private float mDistanceTraveled = 0.0f;
+    private float mTimeWaited = 0.0f;
     private int mSense = 1;
-	// Use this for initialization
+    private bool mWaiting = false;
+	
+    // Use this for initialization
 	void Start () {
-        mDistanceTraveled = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float distanceToMove = mDistance - mDistanceTraveled;
-        if (mSpeed * Time.deltaTime <= distanceToMove)
-            distanceToMove = mSpeed * Time.deltaTime;
-        mDistanceTraveled += distanceToMove;
-        transform.Translate(mSense * mDirection * distanceToMove);
-        if(mDistanceTraveled >= mDistance && mBoomerang)
+        if (!mWaiting)
         {
-            mDistanceTraveled = 0;
-            mSense = mSense == 1 ? -1 : 1;
+            float distanceToMove = mDistance - mDistanceTraveled;
+            if (mSpeed * Time.deltaTime <= distanceToMove)
+                distanceToMove = mSpeed * Time.deltaTime;
+            mDistanceTraveled += distanceToMove;
+            transform.Translate(mSense * mDirection * distanceToMove);
+            if (mDistanceTraveled >= mDistance && mBoomerang)
+            {
+                mDistanceTraveled = 0;
+                mSense = mSense == 1 ? -1 : 1;
+                mWaiting = true;
+            }
         }
+        else
+        {
+            mTimeWaited += Time.deltaTime;
+            if (mTimeWaited >= mWaitTime)
+            {
+                mWaiting = false;
+                mTimeWaited = 0.0f;
+            }
+        }
+        
 	}
 }
