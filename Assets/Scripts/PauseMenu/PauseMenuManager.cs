4dd9ee429de1d87ce39b0b6ed9df88ea2d33551b
estebanarrua <estarr22@hotmail.com>
2017-03-11 16:04:28 +0100

    Add Pause Menu.

---
+++ Assets/Scripts/PauseMenu/PauseMenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour {
+
+    public KeyCode mPauseKey;
+    public GameObject mPausePanel;
+
+    private bool mIsPaused = false;
+	// Use this for initialization
+	void Start () {
+        mPausePanel.SetActive(mIsPaused);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(mPauseKey)){
+            Paused();
+        }
+	}
+
+    void Paused()
+    {
+        mIsPaused = !mIsPaused;
+        mPausePanel.SetActive(mIsPaused);
+        Time.timeScale = mIsPaused ? 0 : 1;
+    }
+
+    public void Resume()
+    {
+        Paused();
+    }
+
+    public void Quit()
+    {
+        Application.LoadLevel(1);
+    }
+}
