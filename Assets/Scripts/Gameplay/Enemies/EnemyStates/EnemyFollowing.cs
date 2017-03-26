a556f6c32a9a0917f1577938f25c7fe2afd40437
Adrián Guevara Alonso <adrianguevaralonso@gmail.com>
2017-03-27 01:42:05 +0200

    Problems with enemy rotations almost solved, added new map to level1 scene

--- Assets/Scripts/Gameplay/Enemies/EnemyStates/EnemyFollowing.cs
+++ Assets/Scripts/Gameplay/Enemies/EnemyStates/EnemyFollowing.cs
@@ -12,6 +12,7 @@ public class EnemyFollowing : EnemyStates {
 	public bool x;
 	public bool y;
 	public bool z;
+	public Vector3 bUp; //a boolean vector
 
 	// Use this for initialization
 	public override void Start () {
@@ -60,6 +61,8 @@ public class EnemyFollowing : EnemyStates {
 		if (player.transform.up.z > 0.5f || player.transform.up.z < -0.5f)
 			z = true;
 
+		bUp = new Vector3 (transform.up.x , transform.up.y, transform.up.z);
+
 		radiusCollider = m_enemy.GetComponent<SphereCollider> ().radius;
 		m_enemy.GetComponent<SphereCollider> ().radius = 0;
 	}
@@ -75,13 +78,33 @@ public class EnemyFollowing : EnemyStates {
 	public void Move()
 	{
 		Vector3 target = player.transform.position;
-		if (x)
+		/*if (x)
 		  	target.x = transform.position.x;
 		if (y)
 			target.y = transform.position.y;
 		if (z)
-			target.z = transform.position.z;
-		transform.LookAt (target);
+			target.z = transform.position.z;*/
+		//transform.LookAt (target);
+
+		Vector3 difference = target - transform.position;
+		if (x) 
+		{
+			float rotationX = Mathf.Atan2 (difference.y, difference.z) * Mathf.Rad2Deg;
+			if (transform.up.x < 0)
+				rotationX *= -1;
+			transform.rotation = Quaternion.Euler (rotationX , m_enemy.currentWall.transform.rotation.eulerAngles.y, m_enemy.currentWall.transform.rotation.eulerAngles.z);
+		}
+		else if (y) 
+		{
+			float rotationY = Mathf.Atan2 (difference.x, difference.z) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.Euler (m_enemy.currentWall.transform.rotation.eulerAngles.x, rotationY, m_enemy.currentWall.transform.rotation.eulerAngles.z);
+		}
+		else if (z) 
+		{
+			float rotationZ = Mathf.Atan2 (difference.x, difference.z) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.Euler (m_enemy.currentWall.transform.rotation.eulerAngles.x, m_enemy.currentWall.transform.rotation.eulerAngles.y, rotationZ);
+		}
+
 		transform.position += transform.forward * speed * Time.deltaTime;
 		//if (y)
 		//	transform.up = new Vector3(transform.up.x, transform.up.y, transform.up.z);
