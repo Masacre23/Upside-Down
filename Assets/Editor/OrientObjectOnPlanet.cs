77c0511fc9b9d1a8a9c59844eff154015c7c5d50
GuillemFP <guillemferre@gmail.com>
2017-04-07 13:39:06 +0100

    Added Tool to align objects on planets with its radius (currently using Z axis of props).

---
+++ Assets/Editor/OrientObjectOnPlanet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class OrientObjectOnPlanet : ScriptableWizard
+{
+    
+    [MenuItem ("Additional Tools/Align radially with father")]
+    static void AlignWithFather()
+    {
+        foreach (GameObject child in Selection.gameObjects)
+        {
+            Transform parentTransform = child.transform.parent;
+            if (parentTransform.tag == "Planet")
+            {
+                Vector3 localPosition = child.transform.localPosition;
+
+                Vector3 radialPosition = child.transform.position - parentTransform.position;
+                Quaternion targetRotation = Quaternion.FromToRotation(child.transform.forward, radialPosition.normalized);
+                child.transform.rotation = targetRotation * child.transform.rotation;
+
+                child.transform.localPosition = localPosition;
+            }
+        }
+    }
+	
+}
