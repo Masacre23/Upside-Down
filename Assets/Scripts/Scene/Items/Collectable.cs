d81a10d831a1d0455d6683a3bb6a12e57796f860
estebanarrua <estarr22@hotmail.com>
2017-04-23 11:28:03 +0200

    Add functionality to items.

---
+++ Assets/Scripts/Scene/Items/Collectable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+    private Player m_player = null;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_player != null)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        int player = LayerMask.NameToLayer("Player");
+        if (col.gameObject.layer == player)
+            m_player = col.gameObject.GetComponent<Player>();
+    }
+}
---
+++ Assets/Scripts/Scene/Items/Oxigem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oxigem : MonoBehaviour {
+    public float m_oxigem = 60;
+
+    private Player m_player = null;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_player != null)
+        {
+            m_player.m_oxigem += m_oxigem;
+            if (m_player.m_oxigem > m_player.m_maxOxigem)
+                m_player.m_oxigem = m_player.m_maxOxigem;
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        int player = LayerMask.NameToLayer("Player");
+        if (col.gameObject.layer == player)
+            m_player = col.gameObject.GetComponent<Player>();
+    }
+}
