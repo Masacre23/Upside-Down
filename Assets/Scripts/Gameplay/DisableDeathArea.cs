e02e282e9005b80cd94f8f94672a976fa5df3640
GuillemFP <guillemferre@gmail.com>
2017-04-25 10:27:32 +0100

    Added level limits for killing respawning player, and script for deactivating limits when needed.

---
+++ Assets/Scripts/Gameplay/DisableDeathArea.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableDeathArea : MonoBehaviour {
+
+    [SerializeField] GameObject m_object;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_object.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_object.SetActive(true);
+        }
+    }
+}
