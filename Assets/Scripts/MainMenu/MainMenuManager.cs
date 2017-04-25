1063da0ca5da323e755e2e2c7aa7c1c9908bb206
estebanarrua <estarr22@hotmail.com>
2017-04-25 15:30:06 +0200

    Presentation screem.

--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -5,23 +5,43 @@ using UnityEngine.EventSystems;
 
 public class MainMenuManager : MonoBehaviour {
 	public EventSystem m_eventSysterm;
+    public GameObject m_canvasLogo;
+    public GameObject m_canvasMenu;
 
     private GameObject m_selected;
+    private bool showLogo = true;
+    private float time = 0.0f;
 
     private void Start()
     {
         m_selected = m_eventSysterm.firstSelectedGameObject;
+        showLogo = true;
     }
 
     private void Update()
     {
-        if (m_eventSysterm.currentSelectedGameObject != m_selected)
+        if (showLogo)
         {
-            if (m_eventSysterm.currentSelectedGameObject == null)
-                m_eventSysterm.SetSelectedGameObject(m_selected);
-            else
-                m_selected = m_eventSysterm.currentSelectedGameObject;
+            time += Time.deltaTime;
+            if(time >= 3.0f)
+            {
+                m_eventSysterm.SetSelectedGameObject(null);
+                showLogo = false;
+                m_canvasLogo.SetActive(false);
+                m_canvasMenu.SetActive(true);
+            }
         }
+        else
+        {
+            if (m_eventSysterm.currentSelectedGameObject != m_selected)
+            {
+                if (m_eventSysterm.currentSelectedGameObject == null)
+                    m_eventSysterm.SetSelectedGameObject(m_selected);
+                else
+                    m_selected = m_eventSysterm.currentSelectedGameObject;
+            }
+        }
+        
     }
 
     public void PressPlayButton(){
