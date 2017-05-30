6ae38ead1a86c24480096c6879ddfae5854106d6
estebanarrua <estarr22@hotmail.com>
2017-05-30 18:55:56 +0200

    Add music in planet.

---
+++ Assets/Scripts/Audio/ChangeMusicLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMusicLevel : MonoBehaviour {
+
+    public AudioClip m_clip;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            AudioManager.Instance().PlayMusic(m_clip, 4.0f);
+        }
+    }
+}
