cb3573ee2fdfbc85549a44040a2c6697cb7093f7
Masacre23 <adrianguevaralonso@gmail.com>
2017-05-31 18:19:51 +0200

    Added dialogue manager and created conversations

---
+++ Assets/Dialogue/DialogueZone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueZone : MonoBehaviour {
+
+    bool playerInside = false;
+    public GameObject dialogue;
+    Player playerManager;
+    bool b = false;
+
+    void Awake()
+    {
+        playerManager = GameObject.Find("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Return) && playerInside && !b)
+        {
+            dialogue.SetActive(true);
+            playerManager.m_negatePlayerInput = true;
+            b = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            playerInside = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            playerInside = false;
+    }
+}
