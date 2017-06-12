d5238bc9baa60cb51f633c959bc56a8682216520
estebanarrua <estarr22@hotmail.com>
2017-06-12 15:24:30 +0200

    Create a new behavior, Floating platforms.

---
+++ Assets/Scripts/Scene/MobilePlatforms/FloatingPlatform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingPlatform : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Vector3 distance = other.transform.position - this.transform.position;
+            this.transform.Rotate(new Vector3(0.0f, distance.x * speed * Time.deltaTime, 0.0f));
+        }
+    }
+}
