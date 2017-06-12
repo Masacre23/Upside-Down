4763346bd2732726f64f545c075849a023cd3266
estebanarrua <estarr22@hotmail.com>
2017-06-12 20:09:42 +0200

    Changes in the level. The fish behavior is fixed.

--- Assets/Scripts/Scene/ParabolicFish.cs
+++ Assets/Scripts/Scene/ParabolicFish.cs
@@ -2,47 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum FishState
+{
+    STOP,
+    WAIT,
+    MOVE,
+}
+
 public class ParabolicFish : MonoBehaviour {
 
     public Transform m_initialPosition;
-    public float m_alpha;
-    public float m_initialSpeed;
+    public float m_XSpeed;
+    public float m_YSpeed;
     public float m_gravity;
+    public float m_wait;
 
     private Vector3 m_initialVector;
-    private float m_sinAlpha;
-    private float m_cosAlpha;
-    private float m_sinBeta;
-    private float m_cosBeta;
     private float m_aceleration;
     private float m_lastSpeed;
-
+    private FishState m_state;
+    private float m_waiting;
 
     // Use this for initialization
     void Start () {
         m_initialVector = m_initialPosition.position;
-        m_sinAlpha = Mathf.Sin(Mathf.Deg2Rad * m_alpha);
-        m_cosAlpha = Mathf.Cos(Mathf.Deg2Rad * m_alpha);
         m_aceleration = m_gravity / 2;
-        m_lastSpeed = m_initialSpeed;
+        m_lastSpeed = m_YSpeed;
+        m_state = FishState.WAIT;
+        m_waiting = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if(m_lastSpeed <= -m_initialSpeed)
+        switch (m_state)
         {
-            transform.position = m_initialVector;
-            m_lastSpeed = m_initialSpeed;
-        }
-        else
-        {
-            Vector3 newPosition = Vector3.zero;
-            newPosition.x = transform.position.x + m_lastSpeed * m_cosAlpha * Time.deltaTime;
-            newPosition.z = transform.position.z;
-            newPosition.y = transform.position.y + m_lastSpeed * m_sinAlpha * Time.deltaTime - m_aceleration * Time.deltaTime * Time.deltaTime;
-            m_lastSpeed = m_lastSpeed * m_sinAlpha - m_gravity * Time.deltaTime;
+            case FishState.WAIT:
+                m_waiting += Time.deltaTime;
+                if(m_waiting >= m_wait)
+                {
+                    m_state = FishState.MOVE;
+                    m_waiting = 0;
+                }
+                break;
+            case FishState.MOVE:
+                if (m_lastSpeed <= -m_YSpeed)
+                {
+                    transform.position = m_initialVector;
+                    m_lastSpeed = m_YSpeed;
+                    m_state = FishState.WAIT;
+                }
+                else
+                {
+                    Vector3 newPosition = Vector3.zero;
+                    newPosition.x = m_XSpeed * Time.deltaTime;
+                    newPosition.y = m_lastSpeed * Time.deltaTime - m_aceleration * Time.deltaTime * Time.deltaTime;
+                    m_lastSpeed = m_lastSpeed - m_gravity * Time.deltaTime;
 
-            transform.Translate(newPosition -transform.position);
+                    transform.Translate(newPosition);
+                }
+                break;
         }
+        
 	}
 }
