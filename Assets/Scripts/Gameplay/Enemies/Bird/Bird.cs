6a0ddb2e6ff6834ec0577634c03ffef6c03f16c4
Adrián Guevara Alonso <adrianguevaralonso@gmail.com>
2017-06-22 18:05:53 +0200

    Created bird with a simple script

---
+++ Assets/Scripts/Gameplay/Enemies/Bird/Bird.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    Vector3 v;
+    public Transform point;
+    public float speed = 20.0f;
+
+	void Start () {
+        v = transform.position - point.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//v = Quaternion.AngleAxis(Time.deltaTime * speed, Vector3.forward) * v;
+      //  transform.position = point.position + v;
+
+        transform.RotateAround(point.position, transform.up, 20 * Time.deltaTime);
+    }
+}
