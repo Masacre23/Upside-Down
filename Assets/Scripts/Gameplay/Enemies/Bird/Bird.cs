3c57ca2083784aba0604e1e441fb5669634940bb
Adrián Guevara Alonso <adrianguevaralonso@gmail.com>
2017-06-23 18:34:01 +0200

    Changes in enemy bird

--- Assets/Scripts/Gameplay/Enemies/Bird/Bird.cs
+++ Assets/Scripts/Gameplay/Enemies/Bird/Bird.cs
@@ -8,15 +8,40 @@ public class Bird : MonoBehaviour {
     public Transform point;
     public float speed = 20.0f;
 
+	enum state
+	{
+		IDLE,
+		ATTACKING
+	};
+
+	state bird_state;
+	Vector3 target;
+
 	void Start () {
         v = transform.position - point.position;
+		bird_state = state.IDLE;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//v = Quaternion.AngleAxis(Time.deltaTime * speed, Vector3.forward) * v;
-      //  transform.position = point.position + v;
+		switch(bird_state)
+		{
+		case state.IDLE:
+			transform.RotateAround (point.position, transform.up, 20 * Time.deltaTime);
+			break;
 
-        transform.RotateAround(point.position, transform.up, 20 * Time.deltaTime);
+		case state.ATTACKING:
+			transform.rotation = Quaternion.Slerp( transform.rotation, Quaternion.LookRotation( target - transform.position ), Time.deltaTime );
+			transform.position += transform.forward * speed * Time.deltaTime;
+			break;
+		}
     }
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") {
+			bird_state = state.ATTACKING;
+			target = other.gameObject.transform.position;
+		}
+	}
 }
