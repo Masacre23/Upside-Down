d8d2c1bf284ca38bcef2c6ba921239fc2e55b155
GuillemFP <guillemferre@gmail.com>
2017-06-29 10:24:28 +0100

    Added tree animation and solved problem with water sound.

--- Assets/Scripts/Effects/EffectBehaviours/FallingOnWaterCubic.cs
+++ Assets/Scripts/Effects/EffectBehaviours/FallingOnWaterCubic.cs
@@ -14,13 +14,26 @@ public class FallingOnWaterCubic : MonoBehaviour
 	
 	void OnTriggerEnter(Collider other)
     {
-        Vector3 direction = other.transform.position - transform.position;
-
-        if (Mathf.Abs(direction.x) >= Mathf.Abs(direction.y))
+        if (!other.isTrigger)
         {
-            if (Mathf.Abs(direction.x) >= Mathf.Abs(direction.z))
+            Vector3 direction = other.transform.position - transform.position;
+
+            if (Mathf.Abs(direction.x) >= Mathf.Abs(direction.y))
             {
-                direction.y = 0.0f;
+                if (Mathf.Abs(direction.x) >= Mathf.Abs(direction.z))
+                {
+                    direction.y = 0.0f;
+                    direction.z = 0.0f;
+                }
+                else
+                {
+                    direction.x = 0.0f;
+                    direction.y = 0.0f;
+                }
+            }
+            else if (Mathf.Abs(direction.y) >= Mathf.Abs(direction.z))
+            {
+                direction.x = 0.0f;
                 direction.z = 0.0f;
             }
             else
@@ -28,26 +41,15 @@ public class FallingOnWaterCubic : MonoBehaviour
                 direction.x = 0.0f;
                 direction.y = 0.0f;
             }
+
+            EffectsManager.Instance.GetEffect(m_prefabEffect, other.transform.position, direction.normalized, transform);
+
+            SoundEffects sound = other.gameObject.GetComponent<SoundEffects>();
+            if (sound)
+                sound.PlaySound("SplashWater");
+
+            if (other.gameObject.layer == LayerMask.NameToLayer("ThrowableObject"))
+                other.gameObject.SetActive(false);
         }
-        else if (Mathf.Abs(direction.y) >= Mathf.Abs(direction.z))
-        {
-            direction.x = 0.0f;
-            direction.z = 0.0f;
-        }
-        else
-        {
-            direction.x = 0.0f;
-            direction.y = 0.0f;
-        }        
-
-        EffectsManager.Instance.GetEffect(m_prefabEffect, other.transform.position, direction.normalized, transform);
-
-        SoundEffects sound = other.gameObject.GetComponent<SoundEffects>();
-        if (sound)
-            sound.PlaySound("SplashWater");
-
-        if (other.gameObject.layer == LayerMask.NameToLayer("ThrowableObject"))
-            other.gameObject.SetActive(false);
-
     }
 }
