4fefdfb542e84f7be67edd169e4979b19d69066d
Adrián Guevara Alonso <adrianguevaralonso@gmail.com>
2017-07-04 13:25:53 +0200

    New "isometric" camera for the planet

---
+++ Assets/Scripts/Cameras/Pruebas/ChangeFaceCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeFaceCamera : MonoBehaviour {
+
+	public int numPoint = -1;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.tag == "Player")
+			Camera.main.transform.parent = GameObject.Find ("Player").transform;
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if (col.tag == "Player") 
+		{
+			Camera.main.transform.parent = null;
+			Camera.main.GetComponent<IsometricCamera> ().actualPoint = numPoint;
+		}
+	}
+}
---
+++ Assets/Scripts/Cameras/Pruebas/IsometricCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricCamera : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject[] lerpPoints;
+	public int actualPoint;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (player.transform, player.transform.up);
+		if(actualPoint != -1 && transform.parent == null)
+			transform.position = Vector3.Lerp (transform.position, lerpPoints [actualPoint].transform.position, Time.deltaTime);
+	}
+}
