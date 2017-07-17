7466d6e9aac5c1a3883285666f390c981b3923cb
Adrián Guevara Alonso <adrianguevaralonso@gmail.com>
2017-07-17 17:00:42 +0200

    New camera for asteroids and a silouette shader for the player

---
+++ Assets/Scripts/Cameras/Pruebas/ActivateCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateCamera : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.transform.tag == "Player") 
+		{
+			Camera.main.GetComponent<IsometricCamera> ().enabled = true;
+			Camera.main.GetComponent<AsteroidCamera> ().enabled = false;
+		}
+	}
+}
