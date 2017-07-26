42aad36dcdc776f20d3aa4e383f771876c64dd9f
GuillemFP <guillemferre@gmail.com>
2017-07-26 13:01:54 +0100

    Changed position of initial asteroids and camera change. Added shadows to birds.

---
+++ Assets/Scripts/Cameras/Pruebas/ActivateCameraOnCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateCameraOnCollision : MonoBehaviour
+{
+    public IsometricCamera m_isometric;
+    public AsteroidCamera m_asteroids;
+
+    public bool m_activateIsometric = true;
+
+    void OnCollisionEnter(Collision col)
+	{
+		if (col.transform.tag == "Player") 
+		{
+            if (m_isometric && m_asteroids)
+            {
+                if (m_activateIsometric)
+                {
+                    m_isometric.enabled = true;
+                    m_asteroids.enabled = false;
+                }
+                else
+                {
+                    m_isometric.enabled = false;
+                    m_asteroids.enabled = true;
+                }
+            }
+		}
+	}
+}
