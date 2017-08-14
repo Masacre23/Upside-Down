4ab635625630048b837ed6994366fe119c44e1d8
Adrián Guevara Alonso <adrianguevaralonso@gmail.com>
2017-08-14 22:10:11 +0200

    Added lights to the scene and a special light with scripts

---
+++ Assets/Scripts/Light/ChangeLightFace.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLightFace : MonoBehaviour {
+
+	public GameObject lt;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.transform.tag == "Player")
+			lt.transform.parent = col.transform;
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if(col.transform.tag == "Player")
+			lt.transform.parent = null;
+	}
+}
