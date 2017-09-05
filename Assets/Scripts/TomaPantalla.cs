2149c07916df620d9dbbc5abac3a9d00e7a51d36
Esteban <estarr22@hotmail.com>
2017-09-05 22:55:24 +0200

    Script for take screenshot.

---
+++ Assets/Scripts/TomaPantalla.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TomaPantalla : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F10))
+        {
+            string screenshotIMGName = System.DateTime.Now.ToString();
+            string subString = screenshotIMGName.Replace('/', '_');
+            string gypsy = subString.Replace(':', '_');
+            Debug.Log("Screen shot captured: " + gypsy + ".png");
+            Application.CaptureScreenshot(gypsy + ".png");
+        }
+    }
+}
