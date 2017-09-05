2dd33221e83177de75c9c68f410e323e5b30fdcf
GuillemFP <guillemferre@gmail.com>
2017-09-05 01:16:31 +0100

    Solved transparency behind objects with stencil shader plus script

---
+++ Assets/Scripts/Cameras/Effects/ActivateStencil.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateStencil : MonoBehaviour
+{
+    public Transform m_raycastTarget;
+    public GameObject m_stencilSphere;
+
+    public bool m_stencilActive = false;
+
+    public LayerMask m_meshesToCheck;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SetStencil(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 toTarget = m_raycastTarget.position - transform.position;
+        bool hasTarget = Physics.Raycast(transform.position, toTarget.normalized, toTarget.magnitude, m_meshesToCheck);
+
+        if (!m_stencilActive && hasTarget)
+            SetStencil(true);
+        else if (m_stencilActive && !hasTarget)
+            SetStencil(false);
+	}
+
+    private void SetStencil(bool value)
+    {
+        if (m_stencilSphere)
+            m_stencilSphere.SetActive(value);
+
+        m_stencilActive = value;
+    }
+}
