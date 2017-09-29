bf4e32f19457558e767819b28e4b36a1e64aacf9
Masacre23 <adrianguevaralonso@gmail.com>
2017-09-29 02:38:57 +0200

    Created an event system for dialogues and added new boss and king to the cinematic

--- Assets/Scripts/Scene/SceneIceManager.cs
+++ Assets/Scripts/Scene/SceneIceManager.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneIceManager : MonoBehaviour {
 
     public GameObject canvas;
+    Image fade;
 
 	// Use this for initialization
 	void Start () {
         canvas.SetActive(true);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        fade = GameObject.Find("Fade").GetComponent<Image>();
+        StartCoroutine(Fade());
+    }
+
+    private IEnumerator Fade()
+    {
+        while (fade.color.a > 0)
+        {
+            Color temp = fade.color;
+            temp.a -= Time.deltaTime / 5;
+            fade.color = temp;
+            yield return 0;
+        }
+    }
 }
